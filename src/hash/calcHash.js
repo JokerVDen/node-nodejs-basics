88c71b145fb32b82a3ab32143880c8f3d981f539
Denis Emelianenko <demelianenko@corcentric.com>
2022-06-03 21:54:54 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,15 @@
+import {readFile} from "node:fs/promises";
+import path from "node:path";
+import dirname from "./dirname.js";
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const { createHash } = await import('crypto');
+    const file = path.resolve(dirname, './files/fileToCalculateHashFor.txt');
+    const content = await readFile(file);
+
+    const hex = createHash('sha256')
+        .update(content)
+        .digest('hex');
+
+    console.log(hex);
 };
