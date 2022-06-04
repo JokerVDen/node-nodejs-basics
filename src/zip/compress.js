3240c397d27ce9229cc43e0fd4d1e2aa3de626c8
Denis Emelianenko <demelianenko@corcentric.com>
2022-06-04 11:08:24 +0300

    feat: zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,13 @@
+import path from "node:path";
+import fs from "node:fs";
+import {createGzip} from "node:zlib";
+import dirname from "./dirname.js";
+
 export const compress = async () => {
-    // Write your code here 
+    const inputFilePath = path.resolve(dirname, './files/fileToCompress.txt');
+    const outputFilePath = path.resolve(dirname, './files/archive.gz');
+
+    const writeStream = fs.createWriteStream(outputFilePath);
+    const gzip = createGzip();
+    fs.createReadStream(inputFilePath).pipe(gzip).pipe(writeStream);
 };
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,13 @@
+import path from "node:path";
+import fs from "node:fs";
+import {createGunzip} from "node:zlib";
+import dirname from "./dirname.js";
+
 export const decompress = async () => {
-    // Write your code here 
+    const inputFilePath = path.resolve(dirname, './files/archive.gz');
+    const outputFilePath = path.resolve(dirname, './files/fileToCompress.txt');
+
+    const writeStream = fs.createWriteStream(outputFilePath);
+    const gzip = createGunzip();
+    fs.createReadStream(inputFilePath).pipe(gzip).pipe(writeStream);
 };
