4c3db3e3623f0031d56ac4750cd90fdb4522e10a
Denis Emelianenko <demelianenko@corcentric.com>
2022-06-04 10:30:16 +0300

    feat: streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,10 @@
+import path from "node:path";
+import fs from "node:fs";
+import {stdout} from "node:process";
+import dirname from "./dirname.js";
+
 export const read = async () => {
-    // Write your code here 
+    const filePath = path.resolve(dirname, './files/fileToRead.txt');
+
+    fs.createReadStream(filePath).pipe(stdout);
 };
